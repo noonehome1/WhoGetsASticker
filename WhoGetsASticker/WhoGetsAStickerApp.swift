b619b3c96e67c8a56dacaa6fe8fa2c3a584baffd
Paul Wagstaff <paul@pwfotos.ca>
2025-11-22 10:22:39 -0700

    Initial Commit

---
+++ WhoGetsASticker/WhoGetsAStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerApp.swift
+//  WhoGetsASticker
+//
+//  Created by Paul Wagstaff on 2025-11-22.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsAStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
